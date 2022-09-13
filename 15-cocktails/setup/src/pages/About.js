840db6238a6efb3c162aeb4065412307ab887897
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2022-09-13 16:32:42 +1000

    Create a fork for react projects training .

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sed
+        expedita, laboriosam blanditiis beatae nulla facere numquam ratione ipsa
+        aliquid tempora nemo rerum obcaecati esse quasi sapiente quae, vel
+        tempore voluptatem neque corrupti minus, alias laborum labore.
+        Accusantium, labore adipisci!
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
